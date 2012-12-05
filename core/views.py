d84c205d56bc721427182c5a66141793234ae8c2
Roman Iskanderov <roman.iskanderov@gmail.com>
2012-12-06 02:14:35 +0400

    Admin ui. Show list for key entities.

--- core/views.py
+++ core/views.py
@@ -5,5 +5,13 @@ def main_page(request):
     return render_to_response('core/main.html')
 
 @permission_required('core.add_insuranceprogramm')
-def admin_insurance_programm(request):
+def admin_insurance_programm_list(request):
     return render_to_response('core/admin/admin_insurance_programm_list.html')
+
+@permission_required('core.add_article')
+def admin_article_list(request):
+    return render_to_response('core/admin/admin_article_list.html')
+
+@permission_required('core.add_clienttype')
+def admin_client_type_list(request):
+    return render_to_response('core/admin/admin_client_type_list.html')
--- polisingos/urls.py
+++ polisingos/urls.py
@@ -7,7 +7,11 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'core.views.main_page', name='home'),
-    url(r'^back/', 'core.views.admin_insurance_programm', name='admin_insurance_programm'),
+    url(r'^back/$', 'core.views.admin_insurance_programm_list', name='admin_home'),
+    url(r'^back/content/programm/$', 'core.views.admin_insurance_programm_list', name='admin_insurance_programm'),
+    url(r'^back/content/client-type/$', 'core.views.admin_client_type_list', name='admin_client_type'),
+    url(r'^back/content/article/$', 'core.views.admin_article_list', name='admin_article'),
+    url(r'^back/content/$', 'core.views.admin_insurance_programm_list', name='admin_insurance_programm'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
