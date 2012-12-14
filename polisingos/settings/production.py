2aab01323fd98f7a850d5ef18b5d7a30518e31aa
Roman Iskanderov <roman.iskanderov@gmail.com>
2012-12-14 17:14:28 +0400

    Wrong pwd fix

--- polisingos/settings/production.py
+++ polisingos/settings/production.py
@@ -13,8 +13,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',# 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'polisingos',                      # Or path to database file if using sqlite3.
-        'USER': 'polisingos',                      # Not used with sqlite3.
-        'PASSWORD': 'avt902119',                  # Not used with sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'zpQCfy14',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
